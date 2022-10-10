b135a2aa757b10bf168d4cfd2f8b78d04f91a345
Thomas Eizinger <thomas@eizinger.io>
2022-10-10 14:54:45 +1100

    Final server version

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,43 +1,114 @@
-use async_std::net::TcpListener;
+use async_std::net::{Incoming, TcpListener, TcpStream};
 use asynchronous_codec::{Framed, LinesCodec};
-use futures::{SinkExt, StreamExt};
-use std::sync::{Arc, Mutex};
-
-#[derive(Default)]
-struct State {
-    num_pings: u64,
-}
+use futures::{future, SinkExt, StreamExt};
+use std::task::{Context, Poll};
+use std::{io, mem};
 
 #[async_std::main]
 async fn main() {
     let listener = TcpListener::bind("127.0.0.1:8000").await.unwrap();
-    let state = Arc::new(Mutex::new(State::default()));
+
+    let mut ping_counter = PingCounter::new(listener.incoming());
 
     loop {
-        let (stream, _) = listener.accept().await.unwrap();
+        future::poll_fn(|cx| ping_counter.poll(cx)).await.unwrap();
+    }
+}
 
-        async_std::task::spawn({
-            let state = state.clone();
-            let mut framed = Framed::new(stream, LinesCodec);
+struct PingCounter<'a> {
+    incoming: Incoming<'a>,
+    num_pings: u64,
+    streams: Vec<SocketState>,
+}
 
-            async move {
-                while let Some(Ok(msg)) = framed.next().await {
-                    match msg.as_str() {
+enum SocketState {
+    ReadyToReceive(Framed<TcpStream, LinesCodec>),
+    Sending {
+        msg: String,
+        socket: Framed<TcpStream, LinesCodec>,
+    },
+    Flushing(Framed<TcpStream, LinesCodec>),
+    Closed,
+    Poisoned,
+}
+
+impl SocketState {
+    fn poll(&mut self, num_pings: &mut u64, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
+        loop {
+            match mem::replace(self, SocketState::Poisoned) {
+                SocketState::ReadyToReceive(mut socket) => match socket.poll_next_unpin(cx)? {
+                    Poll::Ready(Some(line)) => match line.as_str() {
                         "ping\n" => {
-                            let num_pings = {
-                                let mut guard = state.lock().unwrap();
-
-                                guard.num_pings += 1;
-
-                                guard.num_pings
+                            *num_pings += 1;
+                            *self = SocketState::Sending {
+                                msg: format!("{num_pings}\n"),
+                                socket,
                             };
-
-                            framed.send(format!("{num_pings}\n")).await.unwrap();
                         }
-                        _ => {}
+                        _ => {
+                            *self = SocketState::ReadyToReceive(socket);
+                        }
+                    },
+                    Poll::Ready(None) => {
+                        *self = SocketState::Closed;
                     }
+                    Poll::Pending => {
+                        *self = SocketState::ReadyToReceive(socket);
+                        return Poll::Pending;
+                    }
+                },
+                SocketState::Sending { msg, mut socket } => match socket.poll_ready_unpin(cx)? {
+                    Poll::Ready(()) => {
+                        socket.start_send_unpin(msg)?;
+                        *self = SocketState::Flushing(socket);
+                    }
+                    Poll::Pending => {
+                        *self = SocketState::Sending { msg, socket };
+                        return Poll::Pending;
+                    }
+                },
+                SocketState::Flushing(mut socket) => match socket.poll_flush_unpin(cx)? {
+                    Poll::Ready(()) => {
+                        *self = SocketState::ReadyToReceive(socket);
+                    }
+                    Poll::Pending => {
+                        *self = SocketState::Flushing(socket);
+                        return Poll::Pending;
+                    }
+                },
+                SocketState::Poisoned => {
+                    panic!("poisoned")
+                }
+                SocketState::Closed => {
+                    return Poll::Pending;
                 }
             }
-        });
+        }
+    }
+}
+
+impl<'a> PingCounter<'a> {
+    fn new(incoming: Incoming<'a>) -> Self {
+        Self {
+            incoming,
+            num_pings: 0,
+            streams: Default::default(),
+        }
+    }
+
+    fn poll(&mut self, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
+        loop {
+            for stream in &mut self.streams {
+                let _ = stream.poll(&mut self.num_pings, cx)?;
+            }
+
+            if let Poll::Ready(Some(stream)) = self.incoming.poll_next_unpin(cx)? {
+                self.streams
+                    .push(SocketState::ReadyToReceive(Framed::new(stream, LinesCodec)));
+                continue;
+            }
+
+            return Poll::Pending;
+        }
     }
 }
