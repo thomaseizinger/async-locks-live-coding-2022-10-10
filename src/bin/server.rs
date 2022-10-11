2942e4e22f9125ff859c2a5702eb3e54acafb678
Thomas Eizinger <thomas@eizinger.io>
2022-10-11 21:38:51 +1100

    Final version

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,75 +1,116 @@
 use async_std::net::{Incoming, TcpListener, TcpStream};
 use asynchronous_codec::{Framed, LinesCodec};
-use futures::{future, SinkExt, StreamExt};
+use futures::{SinkExt, StreamExt};
+use std::io;
+use std::sync::{Arc, Mutex};
 use std::task::{Context, Poll};
-use std::{io, mem};
 
 #[async_std::main]
 async fn main() {
     let listener = TcpListener::bind("127.0.0.1:8000").await.unwrap();
 
-    let mut ping_counter = PingCounter::new(listener.incoming());
+    let mut ping_counter = PingCounter {
+        num_pings: 0,
+        incoming: listener.incoming(),
+        streams: vec![],
+    };
 
     loop {
-        future::poll_fn(|cx| ping_counter.poll(cx)).await.unwrap();
+        futures::future::poll_fn(|cx| ping_counter.poll(cx))
+            .await
+            .unwrap();
     }
 }
 
 struct PingCounter<'a> {
-    incoming: Incoming<'a>,
     num_pings: u64,
+    incoming: Incoming<'a>,
     streams: Vec<SocketState>,
+}
+
+impl<'a> PingCounter<'a> {
+    fn poll(&mut self, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
+        loop {
+            for stream in &mut self.streams {
+                match stream.poll(&mut self.num_pings, cx) {
+                    Poll::Ready(Ok(())) => {}
+                    Poll::Ready(Err(e)) => {
+                        eprintln!("Stream failed: {e}")
+                    }
+                    Poll::Pending => {}
+                }
+            }
+
+            match self.incoming.poll_next_unpin(cx)? {
+                Poll::Ready(Some(stream)) => {
+                    self.streams
+                        .push(SocketState::ReadyToReceive(Framed::new(stream, LinesCodec)));
+                    continue;
+                }
+                Poll::Ready(None) => return Poll::Ready(Err(io::ErrorKind::BrokenPipe.into())),
+                Poll::Pending => {}
+            }
+
+            return Poll::Pending;
+        }
+    }
 }
 
 enum SocketState {
     ReadyToReceive(Framed<TcpStream, LinesCodec>),
-    Sending {
+    SendMessage {
         msg: String,
         socket: Framed<TcpStream, LinesCodec>,
     },
     Flushing(Framed<TcpStream, LinesCodec>),
-    Closed,
     Poisoned,
 }
 
 impl SocketState {
     fn poll(&mut self, num_pings: &mut u64, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
         loop {
-            match mem::replace(self, SocketState::Poisoned) {
+            match std::mem::replace(self, SocketState::Poisoned) {
                 SocketState::ReadyToReceive(mut socket) => match socket.poll_next_unpin(cx)? {
                     Poll::Ready(Some(line)) => match line.as_str() {
                         "ping\n" => {
                             *num_pings += 1;
-                            *self = SocketState::Sending {
+
+                            *self = SocketState::SendMessage {
                                 msg: format!("{num_pings}\n"),
                                 socket,
-                            };
+                            }
                         }
                         _ => {
                             *self = SocketState::ReadyToReceive(socket);
+                            continue;
                         }
                     },
                     Poll::Ready(None) => {
-                        *self = SocketState::Closed;
+                        return Poll::Ready(Err(io::ErrorKind::BrokenPipe.into()));
                     }
                     Poll::Pending => {
                         *self = SocketState::ReadyToReceive(socket);
                         return Poll::Pending;
                     }
                 },
-                SocketState::Sending { msg, mut socket } => match socket.poll_ready_unpin(cx)? {
-                    Poll::Ready(()) => {
-                        socket.start_send_unpin(msg)?;
-                        *self = SocketState::Flushing(socket);
+                SocketState::SendMessage { msg, mut socket } => {
+                    match socket.poll_ready_unpin(cx)? {
+                        Poll::Ready(()) => {
+                            socket.start_send_unpin(msg)?;
+
+                            *self = SocketState::Flushing(socket);
+                            continue;
+                        }
+                        Poll::Pending => {
+                            *self = SocketState::SendMessage { msg, socket };
+                            return Poll::Pending;
+                        }
                     }
-                    Poll::Pending => {
-                        *self = SocketState::Sending { msg, socket };
-                        return Poll::Pending;
-                    }
-                },
+                }
                 SocketState::Flushing(mut socket) => match socket.poll_flush_unpin(cx)? {
                     Poll::Ready(()) => {
                         *self = SocketState::ReadyToReceive(socket);
+                        continue;
                     }
                     Poll::Pending => {
                         *self = SocketState::Flushing(socket);
@@ -77,38 +118,9 @@ impl SocketState {
                     }
                 },
                 SocketState::Poisoned => {
-                    panic!("poisoned")
-                }
-                SocketState::Closed => {
-                    return Poll::Pending;
+                    unreachable!("poisoned")
                 }
             }
-        }
-    }
-}
-
-impl<'a> PingCounter<'a> {
-    fn new(incoming: Incoming<'a>) -> Self {
-        Self {
-            incoming,
-            num_pings: 0,
-            streams: Default::default(),
-        }
-    }
-
-    fn poll(&mut self, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
-        loop {
-            for stream in &mut self.streams {
-                let _ = stream.poll(&mut self.num_pings, cx)?;
-            }
-
-            if let Poll::Ready(Some(stream)) = self.incoming.poll_next_unpin(cx)? {
-                self.streams
-                    .push(SocketState::ReadyToReceive(Framed::new(stream, LinesCodec)));
-                continue;
-            }
-
-            return Poll::Pending;
         }
     }
 }
